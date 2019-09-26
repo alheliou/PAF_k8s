7b05f9399425e2e41139e73036ac8857009ea7bd
Alice Héliou <alice.heliou@inria.fr>
2019-09-26 09:04:26 +0000

    Add kubeadm, correct rke launch

--- rke/launch_k8s_UI.sh
+++ rke/launch_k8s_UI.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export KUBECONFIG=kube_config_rancher_kubernetes_cluster.yml
+
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
-kube proxy
+
+kubectl proxy
